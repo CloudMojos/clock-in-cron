2496c8f4a4bd96776e48b6e98e5bd67f9b859117
CloudMojos <joshuel.simbulan@gmail.com>
2026-03-24 05:26:36 +0800

    Fix impot

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { Resend } = require("resend");
-require("dotenv").config();
+import { Resend } from "resend";
+import dotenv from "dotenv";
+dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const BASE = "https://penbrothers-ess-api.payrollsolutions.ph";
